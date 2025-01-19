efd9c775dbd953d68f765ad47ac7fdb297c9a984
Joy Sen <joysen.developer@gmail.com>
2025-01-20 01:51:51 +0600

    Updated LMS

--- force-app/main/default/lwc/lMSParent/lMSParent.js
+++ force-app/main/default/lwc/lMSParent/lMSParent.js
@@ -1,5 +1,29 @@
 import { LightningElement } from 'lwc';
 
 export default class LMSParent extends LightningElement {
+
+    _selected = [];
+
+    get options() {
+        return [
+            { label: 'English', value: 'en' },
+            { label: 'German', value: 'de' },
+            { label: 'Spanish', value: 'es' },
+            { label: 'French', value: 'fr' },
+            { label: 'Italian', value: 'it' },
+            { label: 'Japanese', value: 'ja' },
+        ];
+    }
+
+    get defaultOptions(){
+        return ['en', 'de'];
+    }
+
+    get selected() {
+        return this._selected.length ? this._selected : 'none';
+    }
     
+    handleChange(e) {
+        this._selected = e.detail.value;
+    }
 }
